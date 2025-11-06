08b5bfba52f1dac80b389a181484c069d9843306
Marcos023rt <mrodriguezvillalba5@gmail.com>
2025-11-06 13:31:22 +0100

    06/011/2025
    Empezando la UI

---
+++ Trabajo juego/Assets/Marcos/MScripts/UI/UIMenuPincipal.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIMenuPincipal : MonoBehaviour
+{
+    //este scrip gestiona toda la UI del menu principal :)
+    public Button Jugar;
+    public Button Opciones;
+    public Button SalirDelJuego;
+    public Button BNo;
+    public Button BSi;
+    public Button BOpcionesX;
+    public Button BControles;
+    public Button BControlesX;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
