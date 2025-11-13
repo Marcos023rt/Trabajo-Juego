72c7f80b93429be7c77636670b242a7976e021d2
Marcos023rt <mrodriguezvillalba5@gmail.com>
2025-11-13 13:12:33 +0100

    13/11 marcos avance UI

--- Trabajo juego/Assets/Marcos/MScripts/UI/UIMenuPincipal.cs
+++ Trabajo juego/Assets/Marcos/MScripts/UI/UIMenuPincipal.cs
@@ -3,26 +3,126 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using JetBrains.Annotations;
 
 public class UIMenuPincipal : MonoBehaviour
 {
     //este scrip gestiona toda la UI del menu principal :)
-    public Button Jugar;
-    public Button Opciones;
-    public Button SalirDelJuego;
-    public Button BNo;
-    public Button BSi;
-    public Button BOpcionesX;
-    public Button BControles;
-    public Button BControlesX;
+    #region PanelData
+    public GameObject PanelOpciones;
+    public GameObject PanelSalirDelJuego;
+    public GameObject PanelControles;
+    public GameObject CanvasBrillo;
+    #endregion 
+    #region VolumenData
+    private float valorGuardado;
+    public Slider SliderVolumen;
+    public Toggle Tmute;
+    #endregion
+    #region BrilloData
+    private float brilloGuardado;
+    public Image panelBrillo;
+    public Slider brilloSlider;
+    #endregion
     void Start()
     {
-        
+        #region PanelesStart
+        PanelOpciones.SetActive(false);
+        PanelSalirDelJuego.SetActive(false);
+        PanelControles.SetActive(false);
+        DontDestroyOnLoad(CanvasBrillo);
+        #endregion
+        #region VolumenStart
+        float volumenGuardado = PlayerPrefs.GetFloat("volumen", 0.5f);
+        SliderVolumen.value = volumenGuardado;
+        AudioListener.volume = volumenGuardado;
+        Tmute.isOn = (volumenGuardado == 0);
+        #endregion
+        #region BrilloStart
+        brilloSlider.value = PlayerPrefs.GetFloat("brillo", 0.0f);
+        panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.r, panelBrillo.color.r, brilloGuardado);
+        #endregion
+       
     }
-
-    // Update is called once per frame
-    void Update()
+    #region PanelSalida
+    public void SalirJuego()
     {
-        
+        PanelSalirDelJuego.SetActive(true);
     }
+    public void SiSalir()
+    {
+        Application.Quit();
+    }
+    public void NoSalir()
+    {
+        PanelSalirDelJuego.SetActive(false);
+    }
+    #endregion
+    #region PanelOpciones
+    public void Opciones()
+    {
+        PanelOpciones.SetActive(true);
+    }
+    public void SalirOpciones()
+    {
+        PanelOpciones.SetActive(false);
+    }
+    #endregion
+    #region PanelControles
+    public void Controles()
+    {
+        PanelControles.SetActive(true);
+    }
+    public void SalirControles()
+    {
+        PanelControles.SetActive(false);
+    }
+    #endregion
+    #region Volumen
+    public void cambiarValor(float valor)
+    {
+        valorGuardado = valor;
+        PlayerPrefs.SetFloat("volumen", valor); // le damos un valor a la variable unica para que cuando vuelva a entrar ya tenga el nuevo valor
+    }
+    public void comprobar()
+    {
+        if (SliderVolumen.value == 0)
+        {
+            Tmute.isOn = true;
+        }
+    }
+    public void valorSlider()
+    {
+
+        if (Tmute.isOn)
+        {
+            AudioListener.volume = 0;
+            SliderVolumen.value = 0;
+            //mute.isOn = true;
+        }
+        else
+        {
+            AudioListener.volume = SliderVolumen.value;
+            valorGuardado = SliderVolumen.value; //aqui guardamos el nuevo valor que haya tocado el usuario en el slider
+            cambiarValor(valorGuardado); //llamamos a la funcion que cambia el valor de el PlayerPrefs
+            Tmute.isOn = false;
+        }
+    }
+    #endregion
+    #region Brillo
+        public void CambiarBrillo()
+        {
+         brilloGuardado = brilloSlider.value;
+         PlayerPrefs.SetFloat("brillo", brilloGuardado);
+         panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.r, panelBrillo.color.r, brilloSlider.value);
+        }
+    #endregion
+    #region Resolucion
+
+    #endregion
+    #region Calidad
+
+    #endregion
+
+    
 }
