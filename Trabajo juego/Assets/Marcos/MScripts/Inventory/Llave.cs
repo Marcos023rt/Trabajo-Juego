b62c73fb91fb9d2cd9220581e0cfcdd5cb7844e7
Marcos023rt <mrodriguezvillalba5@gmail.com>
2026-03-06 13:32:53 +0100

    06/03, tareas, jueves, viernes y sábado, casi final, solo queda hacer las tareas esenciales para hacer

--- Trabajo juego/Assets/Marcos/MScripts/Inventory/Llave.cs
+++ Trabajo juego/Assets/Marcos/MScripts/Inventory/Llave.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class Llave : MonoBehaviour, interactive
 {
+    public GameObject subidafinal;
     public ScriptableJugador datosJugador;
+    private void Start()
+    {
+        if (datosJugador.llaveEncontrada == false)
+        {
+            subidafinal.SetActive(false);
+        }
+        if (datosJugador.llaveEncontrada == true)
+        {
+            subidafinal.SetActive(true);
+        }
+    }
     public void Interactuar(GameObject jugador)
     {
         Debug.Log("has desbloqueado la llave");
         datosJugador.llaveEncontrada = true;
         this.gameObject.SetActive(false);
+        subidafinal.SetActive(true);
     }
 }
